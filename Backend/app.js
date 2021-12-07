59e0c023dac13c85058f0b6005c949b0911b7050
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-07 20:16:00 +0100

    ajout module+ app

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//Sécurisation des requetes multi origine 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use ((req, res) =>{
+  res.json({message: 'Requete reçue'});
+});
+module.exports = app;
