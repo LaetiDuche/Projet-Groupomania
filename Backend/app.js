b495097b13fa3bad74411c91f49591eb07e2cfe2
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-09 20:28:51 +0100

    ajout data base

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
+
+require('dotenv').config({path: './.env'});
+
+//Importations des routes
+const gifsRoutes = require('./routes/gifs');
+const userRoutes = require('./routes/user');
+
+//Connection à la base de donnée Mysql
+const sequelize = new Sequelize(process.env.CONNECT_DB);
+try {
+  sequelize.authenticate();
+  console.log('Connecté à la base de données MySQL!');
+  sequelize.query(process.env.NAME_DB)
+  .then(([results, metadata]) => {
+    console.log('Base de données créée !');
+  })
+} catch (error) {
+  console.error('Impossible de se connecter, erreur suivante :', error);
+};
 
 //Sécurisation des requetes multi origine 
 app.use((req, res, next) => {
@@ -9,7 +31,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use ((req, res) =>{
-  res.json({message: 'Requete reçue'});
-});
+//Importation du dossier images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(express.json());
+
+app.use('/api/gifs', gifsRoutes);
+app.use('/api/auth', userRoutes);
+
 module.exports = app;
