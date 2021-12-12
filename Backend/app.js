7c82f40704a92fd55479289f4a2d6cb4eb011b62
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-12 19:27:04 +0100

    ajout sequelize cli + models user et gif + connect DB

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const Sequelize = require('sequelize');
-const mysql2 = require('mysql2');
+const { Sequelize } = require('sequelize');
 
 require('dotenv').config({path: './.env'});
 
-//Importations des routes
-const gifsRoutes = require('./routes/gifs');
-const userRoutes = require('./routes/user');
-
 //Connection à la base de donnée Mysql
-const sequelize = new Sequelize(process.env.CONNECT_DB);
-try {
-  sequelize.authenticate();
+
+const sequelize = new Sequelize(process.env.NAME_DB, process.env.USERNAME_DB, process.env.PASS_DB , {
+  dialect: process.env.DIALECT_DB,
+  host: 'localhost',
+  logging: false,
+ });
+
+ try {
+   sequelize.authenticate();
   console.log('Connecté à la base de données MySQL!');
-  sequelize.query(process.env.NAME_DB)
-  .then(([results, metadata]) => {
-    console.log('Base de données créée !');
-  })
 } catch (error) {
   console.error('Impossible de se connecter, erreur suivante :', error);
-};
+}
+
 
 //Sécurisation des requetes multi origine 
 app.use((req, res, next) => {
@@ -31,12 +29,9 @@ app.use((req, res, next) => {
   next();
 });
 
-//Importation du dossier images
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 app.use(express.json());
 
-app.use('/api/gifs', gifsRoutes);
-app.use('/api/auth', userRoutes);
-
+module.exports = sequelize;
 module.exports = app;
