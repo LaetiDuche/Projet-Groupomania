f70e741badb85da4643c454ed204cc1968a03800
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-16 20:22:17 +0100

    modif app

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+/* const { Server } = require('http'); */
 const app = express();
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-require('dotenv').config({path: './.env'});
+/* require('dotenv').config({path: './.env'}); */
+const env = require('dotenv').load(); 
 
 //Importations des routes
 const userRoute = require('./routes/userRoute');
@@ -25,6 +27,14 @@ const sequelize = new Sequelize(process.env.NAME_DB, process.env.USERNAME_DB, pr
 
 
 //Sécurisation des requetes multi origine 
+
+//TEST SERVER
+app.get('/', function(req, res){
+  res.setHeader('Content-type', 'text/html');
+  res.status(200).send('<h1>Bonjour sur mon server </h1>');
+});
+
+//Sécurisation des requetes multi origine
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -32,9 +42,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/api/auth', userRoute);
+app.use('/api/jwt.utils', userRoute);
 
 module.exports = sequelize;
 module.exports = app;
