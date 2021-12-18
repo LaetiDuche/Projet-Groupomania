252bcdb7765c5564ce25cc84e2489ae5eb8b5bad
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-18 18:06:11 +0100

    modifs des models

--- Backend/models/gif.js
+++ Backend/models/gif.js
@@ -1,7 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Gif extends Model {
     /**
@@ -19,7 +19,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Gif.init({
-   
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     attachment: DataTypes.STRING,
