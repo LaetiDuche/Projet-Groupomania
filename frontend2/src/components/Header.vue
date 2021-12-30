f6f0923601acde74769502a909ad54577a8d01e8
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2021-12-30 21:39:58 +0100

    modif header

--- frontend2/src/components/Header.vue
+++ frontend2/src/components/Header.vue
@@ -1,53 +1,54 @@
+<!--
+Header dynamique entre utilisateur non connecté (s'inscrire, se connecter) 
+et utilisateur connecté (menu dropdown: profil, forum, se déconnecter)
+-->
+
 <template>
-  <div>
-    <b-navbar class="d-flex p-2">
-      <b-navbar-brand class="mx-auto">
-        <router-link to="/"
-          ><img
-            src="../assets/icon-left-font.png"
-            alt="logo"
-            height="48"
-            width="190"
-        /></router-link>
-      </b-navbar-brand>
+  <header>
+    <nav class="navbar d-flex p-2">
+      <div class="mx-auto">
+        <router-link to="/">
+          <img src="../assets/icon-left-font.png" alt="logo" height="48" width="190"/>
+        </router-link>
+      </div>
 
       <!--Si l'utilisateur n'est pas connecté-->
-      <b-navbar-nav class="m-auto ms-md-auto my-auto" v-if="id === null">
+      <div class="m-auto ms-md-auto my-auto" v-if="id === null">
         <b-nav-item class="mx-3 justify-content-lg-end" href="#">
           <router-link to="/signup">S'inscrire</router-link>
         </b-nav-item>
+
         <b-nav-item class="mx-2 justify-content-lg-end" href="#">
           <router-link to="/login">Se connecter </router-link>
-          <router-link to="/profil">
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-            </svg>
-            </router-link>
         </b-nav-item>
-      </b-navbar-nav>
+      </div>
 
       <!--Si l'utilisateur est connecté-->
-      <b-navbar-nav class="m-auto ms-md-auto my-auto" v-else>
-        <b-nav-item class="mx-3 justify-content-lg-end" href="#">
-          <router-link to="/forum">Forum</router-link>
-        </b-nav-item>
-        <b-nav-item class="mx-2 justify-content-lg-end" href="#">
-          <router-link to="/profil">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-            </svg>
-          </router-link>
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-navbar>
-  </div>
+      <div class="dropdown dropstart ">
+        <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp" class="rounded-circle" height="22" alt="Avatar" loading="lazy"/>
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <li>
+            <router-link to="/profil" class="dropdown-item">Mon profil</router-link>
+          </li>
+          <li>
+            <router-link to="/forum" class="dropdown-item">Forum</router-link>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#" v-on:click="logout">Se déconnecter</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+
+  /*Changement de header si utilisateur connecté*/ 
   data() {
     return {
       id: "",
@@ -58,32 +59,42 @@ export default {
     console.log(idUser);
     this.id = idUser;
   },
+
+  /*Se déconnecter*/ 
+  methods: {
+    logout(){
+      localStorage.removeItem('token');
+      localStorage.removeItem('Id');
+      localStorage.removeItem('username');
+      localStorage.removeItem('isAdmin');
+
+      /*Retour à la page Home*/ 
+      this.$router.push('http://localhost:8080/');
+    }
+  }
 };
 </script>
 
 <style lang= "scss">
-div {
+header {
   background-color: #ffffff;
 }
 a {
   text-decoration: none;
   color: black;
   font-weight: bold;
-    &:hover{
-      color: #fd2d01;
-    }
+  &:hover {
+    color: #fd2d01;
+  }
 }
-/* a:hover {
-  color: #fd2d01;
-} */
-b-navbar {
+.navbar {
   box-shadow: 0px 4px 4px -3px rgb(53 53 53 / 20%);
 }
-.bi-person-circle{
+.bi-person-circle {
   color: #fd2d01;
-    &:hover{
-      color: black;
-    }
+  &:hover {
+    color: black;
+  }
 }
 @media screen and (max-width: 440px) and (min-width: 320px) {
   b-navbar {
