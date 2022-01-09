b7d462b43a97170769fc506dada65d601dd74a0a
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2022-01-09 01:14:37 +0100

    modif jwt

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+/* const jwt = require('jsonwebtoken');
 require('dotenv').config({path: './.env'});
 
 module.exports = {
@@ -29,4 +29,32 @@ module.exports = {
     }
     return userId;
   }
-}
+} */
+
+
+
+//Option 2
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: './.env'});
+
+module.exports = (req, res, next) =>{
+  try{
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token,process.env.SECRET_TOKEN);
+    const userId = decodedToken.userId;
+
+    //On compare l'id de l'utilisateur pour vérifier son identité
+    
+      //Si l'id est incorrecte
+    if (req.body.userId && req.body.userId !== userId){
+      throw 'User id non valable !';
+
+      //Si l'id est bon, l'uilisateur est connecté
+    }else{
+      next();
+    }
+  }catch(error){
+    res.status(401).json({error: error | 'Requete non authantifiée !'});
+  }
+};
