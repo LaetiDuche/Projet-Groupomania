1281babb33238d428c4577b0dcebc51766c59403
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2022-01-18 18:34:27 +0100

    modif profil vue

--- frontend2/src/views/Profil.vue
+++ frontend2/src/views/Profil.vue
@@ -2,13 +2,12 @@
 Page profil de l'utilisateur
 Possibilité de modifier son username et sa photo profil
 Possibilité de supprimer son compte
-Affichage de toutes ses publications
 -->
 
 <template>
   <div class="profil">
-    <div
-      class="profil-user mx-auto col-10 col-sm-8 col-md-8 mt-4 p-3 rounded-3 shadow">
+    
+      <div class="profil-user mx-auto my-4 col-10 col-sm-8 col-md-6 p-3 rounded-3 shadow">
       <div class="mx-auto d-flex flex-column">
 
         <div>
@@ -16,36 +15,39 @@ Affichage de toutes ses publications
         </div>
 
         <!--Image user-->
+        <form>
         <div class="d-flex">
 
           <!--Si l'utilisateur click pour modifier-->
-          <img class="img-fluid mx-auto rounded-circle w-50 mt-3 shadow" id="preview" alt="image profil" v-if="imagePreview" :src="imagePreview"/>
+          <img class=" img-profil img-fluid mx-auto rounded-circle  mt-3 shadow" id="preview" alt="image profil" v-if="imagePreview" :src="imagePreview"/>
 
           <!--Si l'utilisateur ne click pas pour modifier-->
-          <img v-else class="img-fluid mx-auto rounded-circle w-50 mt-3 shadow" alt="image profil" src="../assets/user-profile.jpg"/>
+          <img v-else class="img-profil img-fluid mx-auto rounded-circle mt-3 shadow" alt="image profil" src="images/user-profile.jpg"/>
 
           <!--Bouton icon modifier-->
           <div @click="btnUpload" width="16" height="16" class="mt-auto">
-            <label classe="label form-label mb-0 " for="gif">
+            <label classe="label form-label mb-0 " for="photo">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square mt-auto" viewBox="0 0 16 16">
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
             </label>
-            <input class="input form-control d-none" id="gif" @change="gifSelected" ref="gifUpload" type="file" name="gif" accept="image/*"/>
+            <input class="input form-control d-none" id="photo" @change="photoSelected" ref="photoUpload" type="file" name="photo" accept="image/*"/>
           </div>
 
         </div>
+        </form>
 
         <!--Username-->
-        <div class="d-flex mx-auto d-block mt-3 text-center">
-          <div class="d-flex">
-
-            <label class="" for="username"></label>
-            <input v-model="username" id="username" name="username" type="text" class="d-flex text-center border-0" placeholder="Username"/>
+        <div class="d-flex mx-auto  mt-3 text-center">
+           
+          <div class="d-flex mx-auto">
+            
+            <label class="d-flex" for="username"></label>
+            <input v-model="username" id="username" name="username" type="text" class=" text-center border-0 w-100" placeholder="Username"/>
 
             <!--Bouton icon modifier-->
-            <div @click="btnUsername" width="16" height="16" class="mt-auto">
+            <div  width="16" height="16" class="mt-auto ms-auto" type="submit">
               <label classe="label form-label" for="username">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -58,19 +60,24 @@ Affichage de toutes ses publications
           </div>
         </div>
 
-        <!--Bouton supprimer mon compte-->
         <div class="d-flex justify-content-around mt-3 text-center flex-wrap">
 
-          <!-- <button class="btn btn-danger btn-sm shadow-sm mt-3" v-on:click="btnDelete" type="submit" value="supprimer">
-            Supprimer mon compte
-          </button> -->
+          <!--Bouton valider mon profil-->
+          <button class="btn btn-danger btn-sm shadow-sm mt-3" @click.prevent="btnValid" type="submit">
+            Valider mon profil
+            <router-link to="/"></router-link>
+          </button>
 
-          <router-link to="/" class="btn btn-danger btn-sm shadow-sm mt-3" v-on:click="btnDelete">Supprimer mon compte</router-link>
+          <!--Bouton supprimer mon compte-->
+          <button class="btn btn-danger btn-sm shadow-sm mt-3" @click="btnDelete" type="submit">
+            Supprimer mon compte
+            <router-link to="/" ></router-link>
+          </button>
 
         </div>
 
       </div>
-    </div>
+   </div>
   </div>
 </template>
 
@@ -81,36 +88,63 @@ export default {
   /*Modification du username*/
   data() {
     return {
-      username: "",
+      user: "",
+      username: localStorage.getItem('username'),
       imagePreview: null,
-    };
+      photo: ""
+    }
   },
+  
   methods: {
+    getProfil(){
+      const id = localStorage.getItem('Id');
+      fetch('http://localhost:3000/api/users/' + id,{
+        headers: {
+          authorization: 'Bearer' + localStorage.getItem('token')
+        }
+      })
+      .then(response => {
+        this.user = response.data;
+      })
+      .catch(err => {
+        this.error("Erreur get" + err.response.status + "" + err.response.statusText);
+         window.location.reload();
+      })
+    },
     /*Pour modifier l'image profil*/
     btnUpload() {
-      this.$refs.gifUpload.click();
+      this.$refs.photoUpload.click()
     },
-    gifSelected(event) {
+    photoSelected(event) {
       this.imagePost = event.target.files[0];
       this.imagePreview = URL.createObjectURL(this.imagePost);
+      
     },
-
-    /*Pour modifier l'username*/
-    btnUsername() {
-      const userId = parseInt(localStorage.getItem("Id"));
+    /*Pour valider les modifications*/
+    btnValid() {
+      const formData = new FormData()
+      console.log(this.imagePost)
+      formData.append('image', this.imagePost)
+      formData.append('username', this.username )
+      /* const userId = parseInt(localStorage.getItem("Id"));
       const dataProfil = JSON.stringify({
         id: userId,
         username: this.username,
-      });
-      async function postProfil(dataProfil) {
+        photo: this.photo
+      }); */
+      
+      async function postProfil(formData) {
+         
         try {
-          const response = await fetch("http://localhost:8080/api/user/:id", {
+          const response = await fetch("http://localhost:3000/api/users/:id", {
             method: "PUT",
             headers: {
-              "content-type": "application/json",
-              authorization: "bearer" + localStorage.getItem("token"),
+              /* 'Content-Type': 'multipart/form-data', */
+              /* 'Content-Type': 'application/x-www-form-urlencoded', */
+              'Authorization': 'bearer ' + localStorage.getItem('token')
             },
-            body: dataProfil,
+            body: formData,
+           
           });
           if (response.ok) {
             const responseId = await response.json();
@@ -119,10 +153,10 @@ export default {
             console.error("Retour du serveur : ", response.status);
           }
         } catch (e) {
-          console.log(e);
+          /* console.log(e); */
         }
       }
-      postProfil(dataProfil);
+      postProfil(formData);
     },
 
     /*Pour supprimer le compte*/
@@ -131,7 +165,7 @@ export default {
       const dataProfil = JSON.stringify({ id: userId });
       async function postProfil(dataProfil) {
         try {
-          const response = await fetch("http://localhost:8080/api/user/:id", {
+          const response = await fetch("http://localhost:3000/api/users/:id", {
             method: "DELETE",
             headers: {
               "content-type": "Application/json",
@@ -145,13 +179,14 @@ export default {
             localStorage.removeItem("Id");
             localStorage.removeItem("token");
             localStorage.removeItem("isAdmin");
+
             this.$router.push('http://localhost:8080/');
             /* window.location.href = "http://localhost:8080/signup#/home"; */
           } else {
             console.error("Retour du serveur : ", response.status);
           }
         } catch (e) {
-          console.log(e);
+          /* console.log(e); */
           confirm(
             "Voulez-vous vraiment supprimer votre compte ? </br> Votre profil et toutes vos publications seront supprimés du forum !"
           );
@@ -166,6 +201,11 @@ export default {
 <style lang="scss">
 .profil {
   background-color: #eeeeee;
+}
+.img-profil{
+  object-fit: cover;
+  height: 240px;
+  width: 240px;
 }
 .profil-user,
 .rounded-circle {
