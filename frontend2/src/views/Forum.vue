64bc2e81d80caab0538160a0db66d739745d65d0
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2022-02-02 20:48:57 +0100

    modif forum

--- frontend2/src/views/Forum.vue
+++ frontend2/src/views/Forum.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="forum">
-
     <!--En-tête pour créer un message-->
-    <div class="formulaire mx-auto col-10 col-md-8 col-lg-6 mt-4 p-3 rounded-3 shadow">
+    <div
+      class="
+        formulaire
+        mx-auto
+        col-11 col-md-8 col-lg-6 
+        p-3
+        my-3
+        my-sm-4
+        rounded-3
+        shadow
+      "
+    >
       <div class="d-flex mx-auto">
-
         <!--Utilisateur-->
         <img
           v-if="photo"
-          :src="photo" 
+          :src="photo"
           class="rounded-circle me-3 shadow"
           height="40"
           alt="Avatar"
@@ -16,10 +25,10 @@
         />
         <img
           v-else
-          src="../assets/user-profile.jpg" 
+          src="../assets/user-profile.jpg"
           class="rounded-circle me-3 shadow"
           height="40"
-          alt="Avatar"
+          alt="avatar"
           loading="lazy"
         />
 
@@ -43,143 +52,200 @@
         forum-publication
         formulaire
         mx-auto
-        col-10 col-md-8 col-lg-6
-        my-4
-        p-3
+        col-11 col-md-8 col-lg-6
+        my-3
+        my-sm-4
         rounded-3
         shadow
       "
-     v-for="gifs in gif"
+      v-for="gifs in gif"
       :key="gifs.id"
-      
-    > 
-
-      <div class="d-flex" >
+    >
+      <div class="d-flex px-3 py-3">
 
         <!--Utilisateur-->
         <div class="my-auto">
-        <img
-          :src="gifs.User.photo" 
-          class="rounded-circle  text-center shadow"
-          height="40"
-          alt="avatar"
-          loading="lazy"
-        />
+          <img
+            v-if="gifs.User.photo"
+            :src="gifs.User.photo"
+            class="rounded-circle text-center shadow"
+            height="40"
+            alt="avatar"
+            loading="lazy"
+          />
+           <img
+              v-else
+              class="rounded-circle shadow"
+              height="40"
+              alt="photo profil"
+              loading="lazy"
+              src="../assets/user-profile.jpg"
+              id="preview"
+            />
         </div>
 
         <!--Username et date de publication-->
-        <div class="d-flex flex-column ">
-        <span class="d-flex flex-column ps-3 justify-content-start fw-bold" id="username">
-          {{ gifs.User.username }}
+        <div class="d-flex flex-column px-3">
+          <span
+            class="d-flex flex-column justify-content-start fw-bold"
+            id="username"
+          >
+            {{ gifs.User.username }}
 
-          <span class="date fw-light "> 
-            Le {{ dateFormat(gifs.createdAt) }}
+            <span class="date fw-light">
+              Le {{ dateFormat(gifs.createdAt) }}
+            </span>
           </span>
-
-        </span>
         </div>
 
         <!-- Bouton supprimer le message pour l'admin et l'utilisateur-->
-        <div class="p-1 ms-auto" style="cursor: pointer" role="button" v-if="userId == gifs.userId || isAdmin === 'true'" @click="btnDelete(gifs.id)"> 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-trash"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-            />
-          </svg>
+        <div
+          class="p-1 ms-auto"
+          style="cursor: pointer"
+          role="button"
+          v-if="userId == gifs.userId || isAdmin === 'true'"
+          @click="btnDelete(gifs.id)"
+        >
+          <i class="bi bi-trash"></i>
         </div>
-       
       </div>
-
 
       <!--Titre et gif du message-->
-      <div >
-        <p class="p-1 my-2 ">{{ gifs.title }}</p>
-        <img class="w-100 d-flex justify-content-center mt-2 " :src="gifs.gifs" />
+      <div>
+        <p class="px-3">{{ gifs.title }}</p>
+        <img
+          class="w-100 d-flex justify-content-center"
+          :src="gifs.gifs"
+        />
       </div>
 
-      <!--bouton Like-->
-      <div class="m-1" >
-        <button          
+      <!--Boutons Like/dislike-->
+      <div class="m-1 d-flex justify-content-end">
+
+        <!--Liker-->
+        <button
           class="btn-like bg-transparent border-0 py-auto d-flex text-align"
           type="submit"
-          @click="(btnClick += 1), (click = !click)" 
+          @click="(btnClick1 = 1), (click1 = !click1)"
         >
+          <i class="bi bi-hand-thumbs-up-fill" v-if="click1 === true"></i>
+          <i class="bi bi-hand-thumbs-up" v-else></i>
 
-          <!--Si liker-->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="currentColor"
-            class="bi bi-hand-thumbs-up-fill"
-            viewBox="0 0 16 16"
-            v-if="click === true" 
-          >
-
-            <path
-              d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"
-            />
-          </svg>
-
-          <!--Si pas liker-->
-          <svg
-           
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="currentColor"
-            class="bi bi-hand-thumbs-up"
-            viewBox="0 0 16 16"
-            v-else
-          > 
-            <path
-              d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z"
-            />
-          </svg>
-
-          <span class="number-like px-1">{{ btnClick }}</span>
+          <span class="number-like px-1">{{ btnClick1 }}</span>
         </button>
+
+        <!--Disliker-->
+        <button
+          class="btn-like bg-transparent border-0 py-auto d-flex text-align"
+          type="submit"
+          @click="(btnClick2 = 1), (click2 = !click2)"
+        >
+          <i class="bi bi-hand-thumbs-down-fill" v-if="click2 === true"></i>
+          <i class="bi bi-hand-thumbs-down" v-else></i>
+
+          <span class="number-like px-1">{{ btnClick2 }}</span>
+        </button>
+      </div>
+
+      <!--Commentaires-->
+      <div class="d-flex flex-column mx-auto m-2 px-3 flex-fill">
+
+        <!--Utilisateur connecté-->
+        <div class="d-flex mb-3 border-bottom pb-3">
+          <img
+            v-if="photo"
+            :src="photo"
+            class="rounded-circle me-3 shadow"
+            height="35"
+            alt="Avatar"
+            loading="lazy"
+          />
+          <img
+            v-else
+            src="../assets/user-profile.jpg"
+            class="rounded-circle me-3 shadow"
+            height="35"
+            alt="avatar"
+            loading="lazy"
+          />
+
+          <!--Ecrire un commentaire-->
+          <form action="" method="post" class="d-flex flex-fill">
+            <label class="form-label" for="comment"></label>
+            <input
+              name="comment"
+              class="form-control p-1 border rounded-pill text-center flex-fill"
+              type="text"
+              placeholder="Ecrire un commentaire ..."
+            />
+          </form>
+        </div>
+
+        <!--Commentaires des autres utilisateurs-->
+        <div class=" mb-3">
+          <div class="d-flex my-auto comment-border border px-2 pb-3 pt-2 border-bottom">
+            <img
+              v-if="gifs.User.photo"
+              :src="gifs.User.photo"
+              class="rounded-circle text-center shadow"
+              height="30"
+              alt="avatar"
+              loading="lazy"
+            />
+            <img
+              v-else
+              class="rounded-circle shadow"
+              height="30"
+              alt="photo profil"
+              loading="lazy"
+              src="../assets/user-profile.jpg"
+              id="preview"
+            />
+            <div class="d-flex flex-column flex-fill ms-3">
+              <span class="d-flex flex-column fw-bold" id="username">
+                {{ gifs.User.username }}
+              </span>
+              <p class="fs-6 fw-light mb-0">{{ gifs.title }}</p>
+
+              <span class="date fw-light ms-auto">
+                Le {{ dateFormat(gifs.createdAt) }}
+              </span>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import moment from 'moment'
-import 'moment/locale/fr'
+import moment from "moment";
+import "moment/locale/fr";
 
 export default {
   name: "Forum",
-  
+
   data() {
     return {
-      isAdmin: '',
-      photo: localStorage.getItem("photo"), 
-      userId: localStorage.getItem('Id'),   
-      gifs:[],
-      Users: [],
-      gif: '',
-      User: '',
+      isAdmin: "",
+      photo: localStorage.getItem("photo"),
+     /*  photo:'', */
+      userId: localStorage.getItem("Id"),
+      gifs: [],
+      gif: "",
+      User: "",
       id: "",
-      gifsId:"" ,
-      btnClick: 0,
-      click: false,
-      images:''
+      gifsId: "",
+      btnClick1: 0,
+      btnClick2: 0,
+      click1: false,
+      click2: false,
+      /* clickDislike: false, */
+      images: "",
     };
   },
-    created: function(){
+  created: function () {
     fetch("http://localhost:3000/api/forum", {
       headers: {
         "content-type": "application/json",
@@ -188,33 +254,36 @@ export default {
     })
       .then((response) => response.json())
       .then((data) => (this.gif = data))
-      .then((data) => (this.User= data));
-      
+      .then((data) => (this.User = data));
   },
   mounted() {
-    this.isAdmin = localStorage.getItem('isAdmin');
-    console.log(this.isAdmin)
+    this.isAdmin = localStorage.getItem("isAdmin");
+    console.log(this.isAdmin);
   },
- 
+
   methods: {
 
     //Supprimer une publication
     btnDelete(id) {
-       let gifId = JSON.stringify({id: this.gifId});
+      let gifId = JSON.stringify({ id: this.gifId });
       async function gifForm(dataForm) {
+        confirm("Voulez-vous vraiment supprimer ce message ?");
         try {
-          const response = await fetch("http://localhost:3000/api/forum/" +id, {
-            method: "DELETE",
-            headers: {
-              "content-type": "application/json",
-              'authorization': "bearer " + localStorage.getItem("token"),
-            },
-            body: dataForm,
-          });
+          const response = await fetch(
+            "http://localhost:3000/api/forum/" + id,
+            {
+              method: "DELETE",
+              headers: {
+                "content-type": "application/json",
+                authorization: "bearer " + localStorage.getItem("token"),
+              },
+              body: dataForm,
+            }
+          );
           if (response.ok) {
-            confirm("Voulez-vous vraiment supprimer ce message ?");
+            
             let responseId = await response.json();
-            console.log(responseId);            
+            console.log(responseId);
             location.reload();
           } else {
             console.error("Retour du serveur : ", response.status);
@@ -224,15 +293,14 @@ export default {
         }
       }
       gifForm(gifId);
-    }, 
-
+    },
 
     // Formatte la date de publication
-    dateFormat(dateValue){
-    if (dateValue) {   
-      return moment(String(dateValue)).format('Do MMMM YYYY à HH:mm');
-    }
-  },
+    dateFormat(dateValue) {
+      if (dateValue) {
+        return moment(String(dateValue)).format("Do MMMM YYYY à HH:mm");
+      }
+    },
   },
 };
 </script>
@@ -244,9 +312,14 @@ export default {
 .date {
   font-size: 0.6rem;
 }
+.bi-hand-thumbs-up,
 .bi-hand-thumbs-up-fill,
-.bi-hand-thumbs-up {
+.bi-hand-thumbs-down-fill,
+.bi-hand-thumbs-down {
   color: #fd2d01;
   margin: auto;
+}
+.comment-border {
+  border-radius: 1rem;
 }
 </style>
