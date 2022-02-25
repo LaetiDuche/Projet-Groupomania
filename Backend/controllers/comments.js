0ee43cb19c482179df4c914e24c0bae4328ce8ad
LaetiDuche <79939699+LaetiDuche@users.noreply.github.com>
2022-02-25 18:22:33 +0100

    modif controller comment

--- Backend/controllers/comments.js
+++ Backend/controllers/comments.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const User = require('../models').User;
 const Comment = require('../models').Comment;
-const Gif = require('../models').Gif;
+/* const Gif = require('../models').Gif; */
 
 //Créer un commentaire
 exports.createComment = (req, res, next) => {
   console.log(req.body)
- /*  const commentObject = req.body; */
+  /*  const commentObject = req.body; */
   // Création d'un nouvel objet commentaire
   const comment = new Comment({
-    userId: req.user.id,
+    userId: req.body.userId,
     gifId: req.params.id,
-    comments: req.params.comments,
+    comments: req.body.comments,
   });
   // Enregistrement de l'objet commentaire dans la base de données
 
   comment.save()
     .then(() => res.status(201).json({ message: 'Commentaire enregistré !' }))
-    .catch(error => res.status(400).json({ error })); 
+    .catch(error => res.status(400).json({ error }));
 
   /* comment.save()
     .then(() => {
@@ -36,14 +36,42 @@ exports.getAllComments = (req, res, next) => {
   /* console.log("Current user id admin => " + req.user.isAdmin) */
   console.log("toto comment")
   Comment.findAll(
-    { where: { gifId: req.params.id,},
-    include: User, include: Gif, 
+    {
+      where: {
+        gifId: req.params.id,
+      },
+      include: [{
+        model: User,
+        attributes: ['id', 'username', 'photo']
+      }],
+      /* include: [{
+        model: Gif,
+        attributes: ['id']
+      }], */
       order: [['createdAt', 'DESC']],
-        
     }
   )
-    .then(comments => res.status(200).json(comments))
-    .catch(error => { console.log(error); res.status(400).json({ error }) });
+    .then((comments) => res.status(200).json(comments))
+    .catch((error) => { console.log(error); res.status(400).json({ error }) });
+};
+
+exports.getOneComment = (req, res, next) => {
+  console.log("getOneComment");
+
+  Comment.findOne(
+    {
+      where: {
+        gifId: req.params.id
+      },
+      include: [{
+        model: User,
+        attributes: ['id', 'username', 'photo']
+      }],
+    })
+    .then((comments) => {
+      res.status(200).json(comments);
+    })
+    .catch((error) => res.status(404).json({ error }));
 };
 
 //Supprimer un commentaire (admin)
